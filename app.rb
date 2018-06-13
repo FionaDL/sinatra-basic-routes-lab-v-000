35fc8163d62ccac479fc19235661bd7f7745b661
Fiona Lapham <fionadlapham@gmail.com>
2018-06-13 10:39:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,7 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Hello, World!"
+  get '/name' do
+    "My name is Fiona"
+  end
+
+  get '/hometown' do 
+    "My hometown is Florence"
+  end
+
+  get '/favorite-song' do 
+    "My favorite song is Diamonds on the Souls of Her Shoes"
   end
 end
